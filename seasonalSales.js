6a3c2a53d891218ae3c75de7b90d956320f174a7
Kristin Caron <krissycaron@gmail.com>
2017-03-21 19:05:16 -0500

    started sales js

--- seasonalSales.js
+++ seasonalSales.js
@@ -1,22 +1,45 @@
-{
-  "categories": [
-    {
-      "id": 1,
-      "name": "Apparel",
-      "season_discount": "Winter",
-      "discount": 0.10
-    },
-    {
-      "id": 2,
-      "name": "Furniture",
-      "season_discount": "Autumn",
-      "discount": 0.25
-    },
-    {
-      "id": 3,
-      "name": "Household",
-      "season_discount": "Spring",
-      "discount": 0.15
-    }
-  ]
+console.log("first line");
+
+var productsString = "";
+var catagoriesString = "";
+var current
+
+
+
+//function alerts for whether json files were atttainable. 
+function executeThisCodeAfterFileLoaded (){
+	console.log("Data loaded!");
+	//This is where you are actually pulling the data for access?
+	// var data = JSON.parse(this.responseText);
+	// products(data);
+	// catagories(data);
+	console.log("data from JSON", data);
 }
+
+function executeThisCodeAfterFileFails (){
+	alert("Data did not load!");
+}
+
+//This will request the data from the products.json
+var products = new XMLHttpRequest();
+products.addEventListener("load", executeThisCodeAfterFileLoaded);
+products.addEventListener("error", executeThisCodeAfterFileFails);
+products.open("GET", "products.json");
+products.send();
+console.log(products);
+
+//this will grab and get JSON data from catagories file 
+var catagories = new XMLHttpRequest();
+catagories.addEventListener("load", executeThisCodeAfterFileLoaded);
+catagories.addEventListener("error", executeThisCodeAfterFileFails);
+catagories.open("GET","catagories.json");
+catagories.send();
+
+console.log("last line");
+
+
+
+
+
+
+
